2e19b742dd80678ee673e30eecd3abc2e7ec4c0e
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-02-29 17:36:58 -0800

    add route for VanDetail page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Vans from './pages/Vans.jsx';
+import VanDetail from './pages/VanDetail.jsx';
 import Footer from './components/Footer.jsx';
 
 /*
@@ -49,6 +50,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
       <Footer />
   </BrowserRouter>
