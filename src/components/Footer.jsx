82d3f288dadd13286af56b1ec1ffb5776c3cac57
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-03-06 16:15:14 -0800

    add Footer to Layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Footer = () => {
+  const nowDate = new Date();
+
   return (
     <footer className="page-footer">
       <div className="center">
-        <p className="page-footer__text">Ⓒ 2022 #VANLIFE</p>
+        <p className="page-footer__text">&#169; {nowDate.getFullYear()} #VANLIFE</p>
       </div>
     </footer>
   );
