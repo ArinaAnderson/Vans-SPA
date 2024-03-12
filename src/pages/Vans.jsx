d8f9a7c92b63ecb56717abfd17d31404f9f859c1
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-03-12 13:39:05 -0700

    replace Links with inputs to filter vans with use of setSearchParams

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -16,6 +16,8 @@ const Vans = () => {
 
   const [searchParams, setSearchParams] = useSearchParams();
 
+  const setFilter = (value) => setSearchParams(value);
+
   const typeFilter = searchParams.get('type');
   const vansToDisplay = typeFilter === null ? vansList : vansList.filter((el) => el.type === typeFilter);
   
@@ -40,27 +42,50 @@ const Vans = () => {
   return (
     <main className="vans">
       <div className="center">
-        <Link to="?type=simple" className="vans__filter-link">Simple</Link>
-        <Link to="?type=rugged" className="vans__filter-link">Rugged</Link>
-        <Link to="?type=luxury" className="vans__filter-link">Luxury</Link>
-        <Link to="." className="vans__filter-link">Clear</Link>
         <h1 className="vans__title title">Explore our van options</h1>
         <div className="vans__filter vans-filter">
           <div className="vans-filter__checkboxes">
             <div className="vans-filter__checkbox">
-              <input className="vans-filter__checkbox-input visually-hidden" type="checkbox" id="simple-vans" value="simple" />
+              <input
+                className="vans-filter__checkbox-input visually-hidden"
+                type="checkbox"
+                id="simple-vans"
+                name="simple"
+                value="simple"
+                onChange={(e) => setFilter({ type: e.target.name })}
+              />
               <label className="vans-filter__checkbox-label vans-filter__checkbox-label--simple" htmlFor="simple-vans">Simple</label>
             </div>
             <div className="vans-filter__checkbox">
-              <input className="vans-filter__checkbox-input visually-hidden" type="checkbox" id="luxury-vans" value="luxury" />
+              <input
+                className="vans-filter__checkbox-input visually-hidden"
+                type="checkbox"
+                id="luxury-vans"
+                name="luxury"
+                value="luxury"
+                onChange={(e) => setFilter({ type: e.target.name })}
+              />
               <label className="vans-filter__checkbox-label vans-filter__checkbox-label--luxury" htmlFor="luxury-vans">Luxury</label>
             </div>
             <div className="vans-filter__checkbox">
-              <input className="vans-filter__checkbox-input visually-hidden" type="checkbox" id="rugged-vans" value="rugged" />
+              <input
+                className="vans-filter__checkbox-input visually-hidden"
+                type="checkbox"
+                id="rugged-vans"
+                name="rugged"
+                value="rugged"
+                onChange={(e) => setFilter({ type: e.target.name })}
+              />
               <label className="vans-filter__checkbox-label vans-filter__checkbox-label--rugged" htmlFor="rugged-vans">Rugged</label>
             </div>
           </div>
-          <button className="vans-filter__reset-btn underlined" type="button">Clear filters</button>
+          <button
+            className="vans-filter__reset-btn underlined"
+            type="button"
+            onClick={() => setFilter({})}
+          >
+            Clear filters
+          </button>
         </div>
         <div className="vans__list">
           <ul className="vans-list">
@@ -100,29 +125,10 @@ const Vans = () => {
 // className="btn btn--orange"
 export default Vans;
 
-// <Link className="link-button" to="/vans">Find your van</Link>
-
-// <Link className={`link-button vans-list__item-btn vans-list__item-btn--${type}`} to={`/vans/${id}`}>{`${type[0].toUpperCase()}${type.substring(1)}`}</Link>
-
-
+// Original filters to filter vans by type with use of Link:
 /*
- <Link
-  className={`vans-list__wrap-link vans-list__wrap-link--${id%2}`}
-  to={`/vans/${id}`}
-  aria-label={`View details for ${name}, priced at $${price} per day`}
->
-  <div className="vans-list__img-box">
-    <img className="vans-list__img van__img" src={imageUrl} width="" height="" alt={`Image of ${name}`} />
-  </div>
-  <div className="vans-list__wrap">
-    <div className="vans-list__item-info">
-      <span className="vans-list__item-title">{name}</span>
-      <span className="vans-list__item-price">
-        {`$${price}`}
-        <span>/day</span>
-      </span>
-    </div>
-    <span className={`btn link-button vans-list__item-btn van-type van-type--${type}`}>{`${type[0].toUpperCase()}${type.substring(1)}`}</span>
-  </div>
-</Link>
+  <Link to="?type=simple" className="vans__filter-link">Simple</Link>
+  <Link to="?type=rugged" className="vans__filter-link">Rugged</Link>
+  <Link to="?type=luxury" className="vans__filter-link">Luxury</Link>
+  <Link to="." className="vans__filter-link">Clear</Link>
 */
