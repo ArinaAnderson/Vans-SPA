e5196fdc5104db8d1819ed54d3c4ef4ef25e59de
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-03-13 21:42:43 -0700

    delete HostVanPhotos2.jsx backup file

--- src/pages/Host/HostVanPhotos2.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect} from 'react';
-import { Link } from "react-router-dom";
-
-const HostVanPhotos = () => {
-  return <h1>HostVanPhotos</h1>
-};
-// className="btn btn--orange"
-export default HostVanPhotos;
