4ddec8bc3e044ebbaa07486350c9ab8e0bff6da3
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-03-26 13:11:28 -0700

    refactor getRequest function

--- api.js
+++ api.js
@@ -1,11 +1,19 @@
 import axios from 'axios';
 
 const getRequest = async (url) => {
-  
-  const reponse = await axios.get(url);
-  const allVans = reponse.data.vans;
-  console.log('FENYA', allVans)
-  return allVans;
+  const response = await axios.get(url);
+  const { data } = response;
+  return data;
+  /*
+  try {
+    const reponse = await axios.get(url);
+    const allVans = reponse.data.vans;
+    console.log('FENYA',res.ok, allVans)
+    return allVans;
+  } catch(e) {
+    throw new Error('FAILED!!!!');
+  }
+  */
 };
 
 export default getRequest;
